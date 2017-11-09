650e95e72f18853ca964876b7a4a7a0b182327f1
PLoraine <loraine.perendevy@gmail.com>
2017-11-09 23:58:02 +0100

    fonction getValue récupère la valeur de l'input Affiche dans l'input le calcul Création de la fonction résultat, récupérer la valeur de l'input et affiche le résultat
    index.html ajout de la fonction resultat sur le bouton egal

--- js/app.js
+++ js/app.js
@@ -1,7 +1,23 @@
 
+//Récupérer les valeurs de sboutons et les affiches dans l'input
+//l'argument id = aux id de l'élément html cliqué
 function getValue(id){
+	//variable number va chercher dans le document un element par son id et stocke sa valeur 
 	var number = document.getElementById(id).value;
 	console.log(number);
+	// variable val va chercher dans le document l'élement avec l'id display et stoke sa valeur 
+	var val = document.getElementById("display").value;
+	console.log("ancienne valeur:"+val);
+	//va chercher dans le document l'élément display et va assigner la var val + la var number
+	document.getElementById("display").value = val + number;
 
 }
 
+
+function resultat(){
+	var val = document.getElementById("display").value;
+	var result = eval(val);
+	console.log(result);
+	document.getElementById("display").value = result;
+
+}
